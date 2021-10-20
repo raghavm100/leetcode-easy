58b11928b385b3a6604c5dd78e61067069f566b0
Raghav Mishra <raghavaddon@gmail.com>
2021-10-20 23:23:13 +0530

    [Raghav|Dev|New Sol] | Added solution to uglyNumber problem.

---
+++ uglyNumber-easy.js
@@ -0,0 +1,25 @@
+// ==== Problem statement: https://leetcode.com/problems/ugly-number/ ====
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    
+    if(n<=0) return false
+    
+    while(true){
+        if(n <= 5) return true
+        
+        if(n % 2 === 0)
+            n = n/2
+        else if(n % 3 === 0)
+            n = n / 3
+        else if(n % 5 === 0)
+            n = n / 5
+        else{
+            return false
+        }
+    }
+    
+};
